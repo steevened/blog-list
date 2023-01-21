da4d7524c4e22306d3fa94e700d380a1873de1ba
steven <alvaradosteven315@gmail.com>
2023-01-20 22:51:49 -0500

    working on testing

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/blogs', (req, res) => {
-  Blog.find({}).then((blogs) => {
+blogsRouter.get('/blogs', async (req, res) => {
+  try {
+    const blogs = await Blog.find({})
     res.json(blogs)
-  })
+  } catch (error) {
+    res.status(400).json(error.message)
+  }
 })
 
-blogsRouter.post('/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-  blog.save().then((result) => {
+blogsRouter.post('/blogs', async (req, res) => {
+  try {
+    const blog = new Blog(req.body)
+    const result = await blog.save()
     res.status(201).json(result)
-  })
+  } catch (error) {
+    res.status(400).json(error.message)
+  }
 })
 
 module.exports = blogsRouter
