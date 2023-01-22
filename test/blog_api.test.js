ad18261989d32929755cd0bb5da4b1c139c2ec43
steven <alvaradosteven315@gmail.com>
2023-01-22 16:52:05 -0500

    refactoring code

--- test/blog_api.test.js
+++ test/blog_api.test.js
@@ -3,41 +3,68 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const blogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 1,
-  },
-]
+const helper = require('./test_helper')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(blogs[0])
+  let blogObject = new Blog(helper.blogs[0])
   await blogObject.save()
-  blogObject = new Blog(blogs[1])
+  blogObject = new Blog(helper.blogs[1])
   await blogObject.save()
+})
+
+test('all blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
 })
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(blogs.length)
+  expect(response.body).toHaveLength(helper.blogs.length)
 })
 
-test('a specific author within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-  const authors = response.body.map((r) => r.author)
+// test('a specific author within the returned blogs', async () => {
+//   const response = await api.get('/api/blogs')
+//   const authors = response.body.map((r) => r.author)
 
-  expect(authors).toContain('Edsger W. Dijkstra')
-})
+//   expect(authors).toContain('Edsger W. Dijkstra')
+// })
+
+// test('a valid blog can be added', async () => {
+//   const newBlog = {
+//     title: 'florida',
+//     author: 'miami',
+//     url: 'sfafsd',
+//     likes: 22,
+//   }
+
+//   await api
+//     .post('/api/blogs')
+//     .send(newBlog)
+//     .expect(201)
+//     .expect('Content-Type', /application\/json/)
+
+//   const response = await api.get('/api/blogs')
+//   const titles = response.body.map((r) => r.title)
+
+//   expect(response.body).toHaveLength(blogs.length + 1)
+//   expect(titles).toContain('florida')
+// })
+
+// test('blog without title is not added', async () => {
+//   const newBlog = {
+//     author: 'miami',
+//     url: 'sfafsd',
+//     likes: 22,
+//   }
+
+//   await api.post('/api/blogs').send(newBlog).expect(400)
+
+//   const response = await api.get('/api/blogs')
+//   expect(response.body).toHaveLength(blogs)
+// })
 
 afterAll(() => {
   mongoose.connection.close()
---
+++ test/test_helper.js
@@ -0,0 +1,26 @@
+const Blog = require('../models/blog')
+
+const blogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 1,
+  },
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map((blog) => blog.toJSON())
+}
+
+module.exports = {
+  blogs,
+  blogsInDb,
+}
