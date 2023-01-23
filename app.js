20e144c22c3f48a94ddfc73888dac1b7dcb6e990
steven <alvaradosteven315@gmail.com>
2023-01-23 18:25:49 -0500

    creating login route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 require('express-async-errors');
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.json());
 // app.use(express.static('build'))
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
